42858fb1f485106bec0e04ff5ecf887f6f2fbab8
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-11-14 20:39:20 +0000

    install sunxi-tools only on sunxi

--- debian-install.sh
+++ debian-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -xe
 
 sudo apt-get install libdata-dump-perl
+uname -a | grep sunxi && \
 apt-cache search --names-only '^sunxi-tools$' | cut -d' ' -f1 | xargs -i sudo apt-get install {}
 apt-cache search --names-only '^raspi-gpio$'  | cut -d' ' -f1 | xargs -i sudo apt-get install {}
