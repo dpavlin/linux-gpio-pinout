10dcc9813638d6337e0396edd0e36803b6c02242
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-23 12:19:13 +0100

    compile dts if needed, display remove hint

--- overlay-load.sh
+++ overlay-load.sh
@@ -1,11 +1,25 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
 dtb=$1
 test -f "$dtb" || ( echo "Usage: $0 overlay.dtb" ; exit 1 )
+
+if echo $dtb | grep '\.dts$' ; then
+	echo "Compile overlay $dtb"
+	dtc -I dts -O dtb -o $dtb.dtb $dtb
+	dtb=$dtb.dtb
+	echo "Created $dtb"
+fi
+
 config=`mount -t configfs | awk '{ print $3 }'`
 name=`basename $1`
+
 dir=$config/device-tree/overlays/$name
+
+# remote overlay to reaload it
 test -d $dir && rmdir $dir
+
 mkdir $dir
 cat $dtb > $dir/dtbo
 cat $dir/status
+
+echo "Remove with: rmdir $dir"
